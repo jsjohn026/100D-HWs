2e95de0a2302e7f812c977a8fbf9f52534e57dd3
Jasmine John <jsjohn026@gmail.com>
2025-02-06 11:47:12 -0600

    set up server for API

--- c38hw/mon-jardin-api/server.js
+++ c38hw/mon-jardin-api/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+const PORT = 8000
 
+// set up the server and response to send file
 app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html') /* look in the same location as this server.js and find this file */
+})
 
+// tell server to listen for requests and confirm in console
+app.listen(PORT, ()=> {
+  console.log(`The server is running on ${PORT}! You better go catch it!`)
 })
