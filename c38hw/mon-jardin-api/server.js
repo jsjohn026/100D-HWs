09330f7019978b3e0af487a85dc66374a9552f25
Jasmine John <jsjohn026@gmail.com>
2025-02-06 12:34:46 -0600

    Finalize server for API

--- c38hw/mon-jardin-api/server.js
+++ c38hw/mon-jardin-api/server.js
@@ -2,12 +2,43 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
-// set up the server and response to send file
+// Create some data
+const plants = {
+  chard: {
+    'name':'Swiss Chard',
+    'cultivar':'Celebration',
+    'maturity':20,
+  },
+  kale: {
+    'name':'Kale',
+    'cultivar':'Lacinato/Dinosaur',
+    'maturity':21,
+  },
+  unknown: {
+    'name':'You should plant something.',
+    'cultivar':'Something yummy.',
+    'maturity':30,
+  }
+}
+
+
+// Set up the server and send file as a response
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html') /* look in the same location as this server.js and find this file */
 })
 
+// Create API that will send JSON objects
+app.get('/api/:plantName', (req, res) => { // colon lets express know that the query parameter is going to be on the url
+  const plantsName = req.params.plantName.toLowerCase();
+
+  if(plants[plantsName]) {
+    res.json(plants[plantsName])
+  } else {
+    res.json(plants['unknown'])
+  }
+})
+
 // tell server to listen for requests and confirm in console
 app.listen(PORT, ()=> {
-  console.log(`The server is running on ${PORT}! You better go catch it!`)
+  console.log(`The server is running on port ${PORT}! You better go catch it!`)
 })
