2fac920af586ab08e03a0dde3e784c397f4b12c0
Jasmine John <jsjohn026@gmail.com>
2025-02-11 11:24:32 -0600

    Created get, post, and delete routes

--- c40hw/plant-ranker/public/js/main.js
+++ c40hw/plant-ranker/public/js/main.js
@@ -1,3 +1,27 @@
 const deleteText = document.querySelectorAll('.fa-trash')
 const thumbText = document.querySelectorAll('.fa-thumbs-up')
 
+Array.from(deleteText).forEach(element => {
+  element.addEventListener('click', deletePlant)
+});
+
+async function deletePlant() {
+  const plantName = this.parentNode.childNodes[1].innerText
+  const varietyName = this.parentNode.childNodes[3].innerText
+  try {
+    const response = await fetch('deletePlant', {
+      method: 'delete', 
+      headers: {'Content-Type': 'application/json'}, 
+      body: JSON.stringify({
+        'plant': plantName, 
+        'variety': varietyName
+      })
+    })
+
+    const data = await response.json()
+    console.log(data)
+    location.reload()
+  } catch (err) {
+    console.log(err)
+  }
+}
