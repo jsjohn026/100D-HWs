91d23777d3bcb07d8eb303370bbcaea33b745a8d
Jasmine John <jsjohn026@gmail.com>
2025-02-10 18:13:06 -0600

    initial commit

---
+++ c40hw/plant-ranker/public/js/main.js
@@ -0,0 +1,3 @@
+const deleteText = document.querySelectorAll('.fa-trash')
+const thumbText = document.querySelectorAll('.fa-thumbs-up')
+
---
+++ c40hw/plant-ranker/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+
+let db, 
+    dbConnectionStr = process.env.DB_STRING, 
+    dbName = 'plants'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+      console.log(`Connected to ${dbName} Database`)
+      db = client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+
+app.listen(process.env.PORT || PORT, ()=>{
+  console.log(`Server running on port ${PORT}`)
+})
